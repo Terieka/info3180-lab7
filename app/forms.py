5c443916e189edd3092a8d0d349f313a7bb3d71e
terieka <terieka@yahoo.com>
2021-05-02 01:39:56 -0500

    exercise 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+  
+class UploadForm(FlaskForm):
+    descriptiom = TextAreaField("Description", [validators.required()])
+    photo = FileField('Image', validators=[FileRequired(),FileAllowed(['jpg','png'], 'Please Upload Images Only!')])
